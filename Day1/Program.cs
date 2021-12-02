6a36430119117de391a22c0100faf38ccc2be96a
rad1mm <rsusuk@gmail.com>
2021-12-02 18:05:57 +0100

    day 1 part2

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Shared.IO;
 using System;
+using System.Linq;
 using Shared;
 
 namespace Day1
@@ -8,16 +9,32 @@ namespace Day1
     {
         static void Main(string[] args)
         {
-            var input = InputReader.Read("C:\\Users\\radovan.susuk\\Documents\\AdventOfCode\\Day1\\input\\input.txt");
+            var stringInputs = InputReader.Read("C:\\Users\\radovan.susuk\\Documents\\AdventOfCode\\Day1\\input\\input.txt");
+            int[] intInputs = stringInputs.Select(i => i.ChangeType<int>()).ToArray();
+
             int biggerNumbers = 0;
 
-            for (int i = 1; i < input.Length; i++)
+            for (int i = 1; i < intInputs.Length; i++)
             {
-                biggerNumbers = input[i - 1].ChangeType<int>() < input[i].ChangeType<int>() ? biggerNumbers + 1 : biggerNumbers;
+                biggerNumbers = intInputs[i - 1] < intInputs[i] ? biggerNumbers + 1 : biggerNumbers;
             }
 
-            Console.WriteLine($"Input array length: {input.Length}");
+            Console.WriteLine($"Input array length: {intInputs.Length}");
+            Console.WriteLine();
+            Console.WriteLine("Part 1:");
             Console.WriteLine($"Bigger consecutive numbers count: {biggerNumbers}");
+            Console.WriteLine();
+            
+            int biggerSums = 0;
+            for (int i = 2; i < intInputs.Length-1; i++)
+            {
+                var firstSum = intInputs[i-2] + intInputs[i-1] + intInputs[i];
+                var secondSum = intInputs[i-1] + intInputs[i] + intInputs[i + 1];
+                biggerSums = firstSum < secondSum ? biggerSums+1 : biggerSums;
+            }
+
+            Console.WriteLine("Part 2:");
+            Console.WriteLine($"Bigger consecutive sums of sliding windows count: {biggerSums}");
             Console.ReadKey();
         }
     }
