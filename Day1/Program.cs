ea7fbce4d06ee0b992813bf801cb2f57c0ec6d2b
rad1mm <rsusuk@gmail.com>
2021-12-02 18:07:33 +0100

    refactoring

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -9,27 +9,26 @@ namespace Day1
     {
         static void Main(string[] args)
         {
-            var stringInputs = InputReader.Read("C:\\Users\\radovan.susuk\\Documents\\AdventOfCode\\Day1\\input\\input.txt");
-            int[] intInputs = stringInputs.Select(i => i.ChangeType<int>()).ToArray();
+            var inputs = InputReader.Read("C:\\Users\\radovan.susuk\\Documents\\AdventOfCode\\Day1\\input\\input.txt").Select(i => i.ChangeType<int>()).ToArray();
 
             int biggerNumbers = 0;
 
-            for (int i = 1; i < intInputs.Length; i++)
+            for (int i = 1; i < inputs.Length; i++)
             {
-                biggerNumbers = intInputs[i - 1] < intInputs[i] ? biggerNumbers + 1 : biggerNumbers;
+                biggerNumbers = inputs[i - 1] < inputs[i] ? biggerNumbers + 1 : biggerNumbers;
             }
 
-            Console.WriteLine($"Input array length: {intInputs.Length}");
+            Console.WriteLine($"Input array length: {inputs.Length}");
             Console.WriteLine();
             Console.WriteLine("Part 1:");
             Console.WriteLine($"Bigger consecutive numbers count: {biggerNumbers}");
             Console.WriteLine();
             
             int biggerSums = 0;
-            for (int i = 2; i < intInputs.Length-1; i++)
+            for (int i = 2; i < inputs.Length-1; i++)
             {
-                var firstSum = intInputs[i-2] + intInputs[i-1] + intInputs[i];
-                var secondSum = intInputs[i-1] + intInputs[i] + intInputs[i + 1];
+                var firstSum = inputs[i-2] + inputs[i-1] + inputs[i];
+                var secondSum = inputs[i-1] + inputs[i] + inputs[i + 1];
                 biggerSums = firstSum < secondSum ? biggerSums+1 : biggerSums;
             }
 
