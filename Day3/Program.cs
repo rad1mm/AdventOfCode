cbb522468a253b3d2460b3e5af1e9416e8dba00b
rad1mm <rsusuk@gmail.com>
2021-12-04 00:01:01 +0100

    refactoring

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -13,6 +13,8 @@ namespace Day3
         static void Main(string[] args)
         {
             var input = InputReader.Read("C:\\Users\\radovan.susuk\\Documents\\AdventOfCode\\Day3\\input\\input.txt");
+
+            //part1
             int[] binaryValuesPerPosition = new int[input[0].Length];
             foreach (var binaryCode in input)
             {
@@ -22,17 +24,12 @@ namespace Day3
                 }
             }
 
-            double halfCount = input.Length / 2;
-            var gammaArray = binaryValuesPerPosition.Select(p => p < halfCount ? "0" : "1").ToArray();
-            var epsylonArray = gammaArray.Select(p => p == "0" ? "1" : "0").ToArray();
-            string binaryGamma = String.Join("", gammaArray);
-            string binaryEpsylon = String.Join("", epsylonArray);
-            int gamma = Convert.ToInt32(binaryGamma, 2);
-            int epsylon = Convert.ToInt32(binaryEpsylon, 2);
+            var gammaArray = binaryValuesPerPosition.Select(p => p < input.Length / 2 ? "0" : "1").ToArray();
+            int gamma = Convert.ToInt32(String.Join("",gammaArray ), 2);
+            int epsilon = Convert.ToInt32(String.Join("", gammaArray.Select(p => p == "0" ? "1" : "0").ToArray()), 2);
 
-            Console.WriteLine($"Power consumption is: {gamma*epsylon}");
+            Console.WriteLine($"Power consumption is: {gamma * epsilon}");
             Console.ReadKey();
-
         }
     }
 }
