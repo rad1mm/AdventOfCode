3b61d28d1460ff28987828d32f7000181e7f2310
rad1mm <rsusuk@gmail.com>
2021-12-04 01:18:40 +0100

    day 3 part2

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -13,9 +13,11 @@ namespace Day3
         static void Main(string[] args)
         {
             var input = InputReader.Read("C:\\Users\\radovan.susuk\\Documents\\AdventOfCode\\Day3\\input\\input.txt");
+            int codeLength = input[0].Length;
 
             //part1
-            int[] binaryValuesPerPosition = new int[input[0].Length];
+
+            int[] binaryValuesPerPosition = new int[codeLength];
             foreach (var binaryCode in input)
             {
                 for (int j = 0; j < binaryCode.Length; j++)
@@ -30,6 +32,44 @@ namespace Day3
 
             Console.WriteLine($"Power consumption is: {gamma * epsilon}");
             Console.ReadKey();
+
+            //part2
+
+            var oxygenGeneratorCodeList = input.ToList();
+            var co2ScrubberCodeList = input.ToList();
+            
+            for (int i = 0; i < codeLength; i++)
+            {
+                oxygenGeneratorCodeList = FindCodesForBitPosition(oxygenGeneratorCodeList, i, GreaterOrEqualThan);
+                co2ScrubberCodeList = FindCodesForBitPosition(co2ScrubberCodeList, i, LessThan);
+            }
+
+            int oxygenGenerator = Convert.ToInt32(String.Join("", oxygenGeneratorCodeList[0]), 2);
+            int co2Scrubber = Convert.ToInt32(String.Join("", co2ScrubberCodeList[0]), 2);
+
+            Console.WriteLine($"Life support rating is: {oxygenGenerator * co2Scrubber}");
+            Console.ReadKey();
+        }
+
+        private static List<string> FindCodesForBitPosition(List<string> inputPositions, int bitPosition, Func<int,double,bool> comparisonStrategy)
+        {
+            if (inputPositions.Count == 1) return inputPositions;
+
+            var newCodeList = inputPositions.Where(code => code[bitPosition] == '1').ToList();
+
+            return comparisonStrategy(newCodeList.Count, inputPositions.Count / (double)2) ?
+                newCodeList :
+                inputPositions.Except(newCodeList).ToList();
+        }
+
+        private static bool LessThan(int first, double second)
+        {
+            return first < second;
+        }
+
+        private static bool GreaterOrEqualThan(int first, double second)
+        {
+            return first >= second;
         }
     }
 }
