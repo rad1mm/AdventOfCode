83096c437285632b3af49c5bf083a3c1b6fdc4f1
rad1mm <rsusuk@gmail.com>
2021-12-06 07:40:41 +0100

    day6 part1&2

---
+++ Day6/Program.cs
@@ -0,0 +1,71 @@
+﻿using Shared.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DomainLogic;
+using Shared;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = InputReader.Read("C:\\Users\\radovan.susuk\\Documents\\AdventOfCode\\Day6\\input\\input.txt")[0].Split(',').ChangeArrayType<int>();
+            //part1
+            var schoolOfFish = new List<Fish>();
+
+            foreach (var noOfDaysLeftUntilSpawning in input)
+            {
+                var newFish = new Fish(noOfDaysLeftUntilSpawning, schoolOfFish);
+            }
+
+            for (int i = 0; i < 80; i++)
+            {
+                var currentFish = schoolOfFish.ToArray();
+                foreach (var fish in currentFish)
+                {
+                    fish.LiveADay();
+                }
+            }
+
+            Console.WriteLine($"Number of fish: {schoolOfFish.Count}");
+            Console.ReadKey();
+
+            //part2
+
+            var countsByTimer = new long[9];
+
+            foreach (var fishTimer in input)
+            {
+                countsByTimer[fishTimer] += 1;
+            }
+
+            for (int i = 0; i < 256; i++)
+            {
+                var newCounter = new long[9];
+                for (int j = 0; j < 9; j++)
+                {
+                    if (j + 1 > 8)
+                    {
+                        newCounter[j] = countsByTimer[0];
+                        continue;
+                    }
+
+                    if (j == 6)
+                    {
+                        newCounter[j] = countsByTimer[j + 1] + countsByTimer[0];
+                        continue;
+                    }
+
+                    newCounter[j] = countsByTimer[j + 1];
+                }
+
+                countsByTimer = newCounter;
+            }
+
+            Console.WriteLine($"Number of fish: {countsByTimer.Sum()}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DomainLogic/Fish.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainLogic
+{
+    public class Fish
+    {
+        private int internalTimer;
+        private List<Fish> schoolOfFish;
+        public Fish(int internalTimer, List<Fish> schoolOfFish)
+        {
+            this.internalTimer = internalTimer;
+            this.schoolOfFish = schoolOfFish;
+            this.schoolOfFish.Add(this);
+        }
+
+        public int Timer => internalTimer;
+
+        public void LiveADay()
+        {
+            internalTimer -= 1;
+            if (internalTimer < 0)
+            {
+                internalTimer = 6;
+                var newFish = new Fish(8, schoolOfFish);
+            }
+        }
+    }
+}
