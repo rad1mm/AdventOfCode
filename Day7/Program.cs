b2802a4df50f4f3f5c78bcdc904edd3dc4c84c1a
rad1mm <rsusuk@gmail.com>
2021-12-07 06:45:26 +0100

    day7 part1&2

---
+++ Day7/Program.cs
@@ -0,0 +1,33 @@
+﻿using Shared.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared;
+
+namespace Day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = InputReader.Read("C:\\Users\\radovan.susuk\\Documents\\AdventOfCode\\Day7\\input\\input.txt")[0].Split(',').ChangeArrayType<int>();
+            int minPositionDistance = Int32.MaxValue;
+
+            for (int i = input.Min(); i <= input.Max(); i++)
+            {
+                //int cumulativeDistanceToPosition = input.Select(position => Math.Abs(position - i)).Sum(); part1
+                int cumulativeDistanceToPosition = input.Select(position => Math.Abs(position - i)).Select(nPlaces => (int)(nPlaces / (double)2 * (nPlaces + 1))).Sum(); // part2
+
+                if (cumulativeDistanceToPosition < minPositionDistance)
+                {
+                    minPositionDistance = cumulativeDistanceToPosition;
+                }
+            }
+
+            Console.WriteLine($"Fuel spent: {minPositionDistance}");
+            Console.ReadKey();
+        }
+    }
+}
